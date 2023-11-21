604ddaf9f69ec754007dd9ee0cf19dcb597a2d81
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-21 12:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function changes(){
+	//before create ,append need to remove the p tag
+	const deleElePTag=document.querySelector("#status");
+	deleElePTag.remove();
+	//step 1 create the tagElement
+let headingTag=document.createElement("h1");
+//step -2 set the properties
+headingTag.innerText="Enter Metaverse";
+// step -3 create the parentnode obj, attach to the body(parent node)
+let parentElement=document.querySelector("body");
+//append the childelement to the parent
+//parentElement.appendChild(headingTag);
+// Append the child element to the parent
+parentElement.insertBefore(headingTag, document.getElementById("enterBtn"));
+	return parentElement;
+}
